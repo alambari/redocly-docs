1f32ac354d818b7b4c39df88171216c11d471819
Al Ambari <al.ambari@usetada.com>
2022-02-10 08:16:27 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+//add the router
+app.use('/', router);
+app.use(express.static('public'))
+app.listen(process.env.port || 5002);
+
+console.log('Running at Port 5002');
